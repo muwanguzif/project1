debdeb9811390379c6cd1a519e3d24a7f25ac752
muwanguzif <muwanguzifreedomm@gmail.com>
2019-02-11 18:28:17 +0300

    dd

---
+++ python 1/python.py
@@ -0,0 +1,18 @@
+class employee:
+ def __init__(self,first,last,pay):
+     self.first=first
+     self.last=last
+     self.pay=pay
+     self.email=first + last + @company.com
+
+  def fullname(self):
+      return"{} {}".format (self.last, self.first)
+
+      emp1 = employee("cor","sar",12)
+      emp2 = employee("wer","asd"45)
+
+ print (emp1.fullname())
+
+
+    
+    
